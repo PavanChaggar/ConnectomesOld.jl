1e7856c9cfa3e926cadaa401f49de7df873ba79e
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2021-05-15 12:23:22 +0100

    adding another test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Connectomes
 using Test
-
+using SparseArrays
 
 @testset "Connectomes.jl" begin
     connectome_path = "/"*relpath((@__FILE__)*"/../..","/") * "/assets/connectomes/hcp-scale1-standard-master.graphml"
@@ -8,4 +8,6 @@ using Test
     connectome = Connectome(connectome_path)
 
     @test typeof(connectome) == Connectome
+    @test typeof(connectome.A) == SparseMatrixCSC{Float64, Int64}
+
 end
